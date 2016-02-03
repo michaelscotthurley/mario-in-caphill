165a83dad6be71dcc3381aad7d851eef9fbdb6c4
michaelscotthurley <michaelscotthurley>
2016-02-03 15:52:24 -0800

    Game play implemented and UI updates

--- js/script.js
+++ js/script.js
@@ -3,21 +3,32 @@ var cardUp = $('.back');
 var cardDownImage = "<img src=../mario-in-caphill/img/carddown.png>"
 var cardsUp=[];
 
-var availableItems = [{item: 'beer', imageLink: '../mario-in-caphill/img/rsz_beer.jpg'}, 
-					{item: 'beardcomb', imageLink: '../mario-in-caphill/img/rsz_beardcomb.jpg'},
-					{item: 'orca', imageLink: '../mario-in-caphill/img/rsz_1orca.jpg'}, 
-					{item: 'donuts', imageLink:	'../mario-in-caphill/img/rsz_donuts.jpg'}, 
-					{item: 'coffee'	, imageLink: '../mario-in-caphill/img/rsz_coffee.jpg'},
-					{item: 'tacos', imageLink:	'../mario-in-caphill/img/rsz_tacos.jpg'}];
+var availableItems = ['../mario-in-caphill/img/rsz_beer.jpg',
+					'../mario-in-caphill/img/rsz_beardcomb.jpg',
+					'../mario-in-caphill/img/rsz_1orca.jpg', 
+					'../mario-in-caphill/img/rsz_donuts.jpg', 
+					'../mario-in-caphill/img/rsz_coffee.jpg',
+					'../mario-in-caphill/img/rsz_tacos.jpg'];
 var combinedItems=[]
-var firstCard = [];
-var secondCard = [];
+var firstClick;
+var secondClick;
+var firstImage;
+var secondImage;
 var playerTurn=1;
-var flipCounter = 0;
+var clickCounter=0;
 
 $(document).ready(function() {
 
+$('.gamecard').flip( {
+	trigger: "click",
+	axis: "y"
+});
+
+populateBoard();
+
 $('#start-game').click(function() {
+	combinedItems=[];
+	cardsUp=[];
 	populateBoard();
 });
 
@@ -32,6 +43,7 @@ function populateBoard() {
 		$(this).html("<img src=" + cardsUp[i] + ">");	
 		});
 	}
+
 	console.log(cardsUp);
 }
 
@@ -39,8 +51,8 @@ function populateBoard() {
 //essentially this doubles each item in the array
 function combineItems() {
 	for (var i = 0; i < availableItems.length; i++) {
-			combinedItems.push(availableItems[i].imageLink);
-			combinedItems.push(availableItems[i].imageLink);
+			combinedItems.push(availableItems[i]);
+			combinedItems.push(availableItems[i]);
 	}
 		// console.log(combinedItems);
 		return combinedItems;
@@ -60,9 +72,35 @@ function shuffleItems(array) {
   return cardsUp;
 }
 
-$('.gamecard').flip( {
-	trigger: "click",
-	axis: "y"
-});
-
+//function to flip cards and determine matches
+$('div.gamecard .front').click(function() {
+	clickCounter++
+	if (clickCounter % 2 !== 0) {
+		firstClick = $(this).siblings().html();
+		firstImage = $(this);
+		console.log(firstClick);
+		console.log(firstImage);
+	}
+	else {
+		secondClick = $(this).siblings().html();
+		secondImage = $(this);
+		console.log(secondClick);
+		console.log(secondImage);
+			if (firstClick == secondClick) {
+				firstImage.parent().flip(true);
+				secondImage.parent().flip(true);
+				firstImage.parent().off('.flip');
+				secondImage.parent().off('.flip');
+			//add a sweet alert for matches
+			console.log('Match!');
+		}
+			else {
+				setTimeout(function() {
+				console.log('Sorry, try again!');
+				firstImage.parent().flip('toggle');
+				secondImage.parent().flip('toggle');
+				}, 1000);
+		}
+	}
+	})
 });
